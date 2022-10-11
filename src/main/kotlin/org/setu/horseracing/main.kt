b7d7c73ac489676ba6434749cd6f369dacaf6725
Robert Jacob <Robertjacobios@gmail.com>
2022-10-11 14:38:45 +0100

    added logging

---
+++ src/main/kotlin/org/setu/horseracing/main.kt
@@ -0,0 +1,10 @@
+package org.setu.horseracing
+
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
+fun main(args: Array<String>){
+    logger.info { "Launching Horse racing App" }
+    println("Horse Racing App Version 1.0")
+}
