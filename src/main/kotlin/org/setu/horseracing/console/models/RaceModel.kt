ce59bf29f1abd52bf700843868a5c715e19a01c1
Robert Jacob <Robertjacobios@gmail.com>
2022-10-11 15:15:35 +0100

    added RaceModel

---
+++ src/main/kotlin/org/setu/horseracing/console/models/RaceModel.kt
@@ -0,0 +1,13 @@
+package org.setu.horseracing.console.models
+
+data class RaceModel(var id: Long = 0,
+                     var raceName: String = "",
+                     var raceDate: String = "",
+                     var startTime: String = "",
+                     var venue: String = "",
+                     var raceSize: Int = 0,
+                     var raceWinner: String  = "",
+                     var horsesInRace: Array<String> = arrayOf(",")
+){
+
+}
