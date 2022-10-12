c57f15018f8dbacda4b4c97a8d14ed358ce65b00
Robert Jacob <Robertjacobios@gmail.com>
2022-10-12 16:03:01 +0100

    added HorseStore

---
+++ src/main/kotlin/org/setu/horseracing/console/models/horse/HorseStore.kt
@@ -0,0 +1,9 @@
+package org.setu.horseracing.console.models.horse
+
+
+interface HorseStore {
+    fun findAll(): List<HorseModel>
+    fun findOne(id: Long): HorseModel?
+    fun create(horse: HorseModel)
+    fun update(horse: HorseModel)
+}
